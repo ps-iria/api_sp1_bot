d24d2ffd673ab88557a2c61dd8a46af434812934
илья михайлов <ps.iria@yandex.ru>
2020-11-25 22:25:35 +0700

    Mock Test

--- homework.py
+++ homework.py
@@ -18,6 +18,11 @@ logging.basicConfig(filename='log.txt',
                     level=logging.ERROR,
                     format='%(name)s - %(asctime)s - %(message)s'
                     )
+verdicts = {
+    'rejected': 'К сожалению в работе нашлись ошибки.',
+    'approved': 'Ревьюеру всё понравилось, '
+                'можно приступать к следующему уроку.'
+}
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
@@ -26,15 +31,12 @@ def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
 
-    if homework_status == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-
-    elif homework_status == 'approved':
-        verdict = ('Ревьюеру всё понравилось, '
-                   'можно приступать к следующему уроку.')
-    else:
-        verdict = 'Статус проверки определить не удалось.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    if homework_status not in verdicts:
+        logging.error(f'Статус работы указан неверно, '
+                      f'status: {homework_status}.')
+        return 'Статус проверки определить не удалось.'
+    return (f'У вас проверили работу "{homework_name}"!'
+            f'\n\n{verdicts[homework_status]}')
 
 
 def get_homework_statuses(current_timestamp):
@@ -43,12 +45,12 @@ def get_homework_statuses(current_timestamp):
         "from_date": current_timestamp,
     }
     try:
-        response = requests.get(PRAKTIKUM_URL, headers=headers,
-                                params=data)
-        return response.json()
+        response = requests.get(PRAKTIKUM_URL, headers=headers, params=data)
     except Exception as e:
-        logging.error(f'Бот столкнулся с ошибкой: {e}')
-
+        logging.error(f'Бот столкнулся с ошибкой: {e}, '
+                      f'по запросу {PRAKTIKUM_URL}, с параметрами: {data}')
+        return f'Бот столкнулся с ошибкой: {e}'
+    return response.json()
 
 
 def send_message(message, bot_client):
@@ -57,7 +59,6 @@ def send_message(message, bot_client):
 
 def main():
     current_timestamp = int(time.time())
-
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
@@ -69,25 +70,10 @@ def main():
             current_timestamp = new_homework.get('current_date',
                                                  current_timestamp)
             time.sleep(600)  # 10 минут
-
         except Exception as e:
-            logging.error(f'Бот столкнулся с ошибкой2: {e}')
+            logging.error(f'Бот столкнулся с ошибкой: {e}')
             time.sleep(5)
 
 
 if __name__ == '__main__':
-    from unittest import TestCase, mock
-    import unittest
-
-    ReqEx = requests.exceptions.RequestException  # Короткое имя для ожидаемого исключения
-
-    # main()
-    class TestReq(TestCase):  # Часть трюка
-        @mock.patch('requests.get')  # Указание, что будем подменять requests.get
-        def test_raised(self, rq_get):  # Второй параметр - это подмена для requests.get
-            rq_get.side_effect = mock.Mock(  # Главный трюк - настраиваем подмену, чтобы
-                side_effect=ReqEx('testing'))  # бросалось это исключение
-            main()  # Все подготовили, запускаем
-
-
-    unittest.main()
+    main()
